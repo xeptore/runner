b0206ce76c1d4a46ac1f173961471aedee4e4e53
xeptore <29199390+xeptore@users.noreply.github.com>
2024-02-23 00:25:07 +0330

    simplify iptables script
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- iptables-set.sh
+++ iptables-set.sh
@@ -1,41 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-set -Eeuxo pipefail
+set -x
 
-ip route add local default dev lo table 100
-ip rule add fwmark 1 table 100
+ip -4 route add 0.0.0.0/0 dev tun0 table 51821
+ip -4 rule add not fwmark 2 table 51821
+ip -4 rule add table main suppress_prefixlength 0
+ip -6 route add ::/0 dev tun0 table 51821
+ip -6 rule add not fwmark 2 table 51821
+ip -6 rule add table main suppress_prefixlength 0
 
-iptables -t mangle -N XRAY
-iptables -t mangle -A XRAY -d 10.0.0.0/8 -j RETURN
-iptables -t mangle -A XRAY -d 100.64.0.0/10 -j RETURN
-iptables -t mangle -A XRAY -d 127.0.0.0/8 -j RETURN
-iptables -t mangle -A XRAY -d 169.254.0.0/16 -j RETURN
-iptables -t mangle -A XRAY -d 172.16.0.0/12 -j RETURN
-iptables -t mangle -A XRAY -d 192.0.0.0/24 -j RETURN
-iptables -t mangle -A XRAY -d 224.0.0.0/4 -j RETURN
-iptables -t mangle -A XRAY -d 240.0.0.0/4 -j RETURN
-iptables -t mangle -A XRAY -d 255.255.255.255/32 -j RETURN
-iptables -t mangle -A XRAY -d 192.168.0.0/16 -p tcp -j RETURN
-iptables -t mangle -A XRAY -d 192.168.0.0/16 -p udp -j RETURN
-iptables -t mangle -A XRAY -p tcp -j TPROXY --on-ip 127.0.0.1 --on-port 12345 --tproxy-mark 1
-iptables -t mangle -A XRAY -p udp -j TPROXY --on-ip 127.0.0.1 --on-port 12345 --tproxy-mark 1
-iptables -t mangle -A PREROUTING -j XRAY
-
-iptables -t mangle -N XRAY_SELF
-iptables -t mangle -A XRAY_SELF -d 10.0.0.0/8 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 100.64.0.0/10 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 127.0.0.0/8 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 169.254.0.0/16 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 172.16.0.0/12 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 192.0.0.0/24 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 224.0.0.0/4 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 240.0.0.0/4 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 255.255.255.255/32 -j RETURN
-iptables -t mangle -A XRAY_SELF -d 192.168.0.0/16 -p tcp -j RETURN
-iptables -t mangle -A XRAY_SELF -d 192.168.0.0/16 -p udp -j RETURN
-iptables -t mangle -A XRAY_SELF -m mark --mark 2 -j RETURN
-iptables -t mangle -A XRAY_SELF -p tcp -j MARK --set-mark 1
-iptables -t mangle -A XRAY_SELF -p udp -j MARK --set-mark 1
-iptables -t mangle -A OUTPUT -j XRAY_SELF
-
-echo 'nameserver 1.1.1.2' > /etc/resolv.conf
+cat >/etc/resolv.conf <<EOF
+nameserver 1.1.1.2
+EOF
