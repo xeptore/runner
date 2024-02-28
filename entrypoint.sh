9967b6c1752817b30ec136e53181574031ca9731
xeptore <29199390+xeptore@users.noreply.github.com>
2024-02-28 14:05:45 +0330

    improve logging
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,10 @@ exec_cmd() {
   exec_cmd_nobail "$1" || bail
 }
 
+echo "Starting up on host $(hostname)..."
+
 if [[ -n "${PROXY_SOCKS_HOST}" && -n "${PROXY_SOCKS_PORT}" ]]; then
+  echo 'Starting proxy client...'
   jq \
     --arg proxy_host "$PROXY_SOCKS_HOST" \
     --arg proxy_port "$PROXY_SOCKS_PORT" \
@@ -23,15 +26,34 @@ if [[ -n "${PROXY_SOCKS_HOST}" && -n "${PROXY_SOCKS_PORT}" ]]; then
     '.outbounds[0] += {server: $proxy_host, server_port: ($proxy_port | tonumber), username: $proxy_user, password: $proxy_pass}' \
     /root/sing-box/config.template.json > /root/sing-box/config.json
   EOB
-  exec_cmd '/root/sing-box/sing-box run -c /root/sing-box/config.json' &
+
+  exec_cmd '/root/sing-box/sing-box run -c /root/sing-box/config.json &'
   singbox_pid=$!
-  sleep 2
+  echo "Spawned proxy client in the backgroung with pid $singbox_pid"
+
+  max_retries=10
+  attempt=1
+  while (( attempt <= max_retries )); do
+    if ! (cat /sys/class/net/tun0/operstate); then
+      echo 'Proxy tunnel device is not ready yet. Retrying in 1 second...'
+      sleep 1
+      attempt=$(( attempt + 1 ))
+    else
+      break
+    fi
+  done
+  if [[ attempt -gt max_retries ]]; then
+    echo "Failed to wait for proxy tunnel device to become ready after $attempt retries."
+    exit 69
+  fi
+
   exec_cmd '/root/sing-box/iptables-set.sh'
-  trap 'kill -INT $singbox_pid && wait -fn $singbox_pid' EXIT ERR HUP INT QUIT TERM ABRT
+  trap 'echo "Killing proxy client server..." && exec_cmd "kill -INT $singbox_pid && wait -fn $singbox_pid"' EXIT ERR HUP INT QUIT TERM ABRT
 fi
 
 REG_TOKEN=$(curl -sLX POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/${REPO}/actions/runners/registration-token" | jq .token --raw-output)
 
+echo 'Registering runner...'
 exec_cmd "su nonroot -c './config.sh --unattended --url https://github.com/${REPO} --token ${REG_TOKEN} --ephemeral --labels self-hosted --replace --name $(hostname)'"
 
 cleanup() {
@@ -81,12 +103,14 @@ trap '[[ -f /var/run/docker.pid ]] && kill -INT "$(cat /var/run/docker.pid)" && 
   fi
 )
 
+echo 'Starting runner...'
 exec_cmd 'su nonroot -c ./run.sh'
 cleanup
 
 kill -INT "$(cat /var/run/docker.pid)" && wait -fn $dockerd_entrypoint_pid
 
 if [[ -n "${singbox_pid}" ]]; then
+  echo 'Killing proxy client server...'
   kill -INT "$singbox_pid" && wait -fn "$singbox_pid"
 fi
 
