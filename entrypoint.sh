e5fdf12df40083e6b0c71b7c043d84d2edc904ee
xeptore <29199390+xeptore@users.noreply.github.com>
2024-03-04 10:48:49 +0330

    fix typo
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -87,7 +87,7 @@ su nonroot -c "./config.sh --unattended --url https://github.com/${REPO} --token
 echo '> Runner registered.'
 
 clear_runner() (
-  set -eEuo pipfail
+  set -eEuo pipefail
   echo '> Running runner cleanup...'
   echo '> Obtaining runner remove token...'
   remove_token=$(curl -1sSfLX POST -H 'Accept: application/vnd.github+json' -H "Authorization: Bearer ${ACCESS_TOKEN}" -H 'X-GitHub-Api-Version: 2022-11-28' "https://api.github.com/repos/${REPO}/actions/runners/remove-token" | jq .token --raw-output)
