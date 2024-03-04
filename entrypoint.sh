6453a790023811d7ba77afbb1d4e043ced69d2b9
xeptore <29199390+xeptore@users.noreply.github.com>
2024-03-04 10:00:19 +0330

    use runner remove token for cleanup
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -80,17 +80,23 @@ if [[ -n "${PROXY_SOCKS_HOST}" && -n "${PROXY_SOCKS_PORT}" ]]; then
   /root/sing-box/iptables-set.sh
 fi
 
-reg_token=$(curl -sLX POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/repos/${REPO}/actions/runners/registration-token" | jq .token --raw-output)
+reg_token=$(curl -1sSfLX POST -H 'Accept: application/vnd.github+json' -H "Authorization: Bearer ${ACCESS_TOKEN}" -H 'X-GitHub-Api-Version: 2022-11-28' "https://api.github.com/repos/${REPO}/actions/runners/registration-token" | jq .token --raw-output)
 
 echo '> Registering runner...'
 su nonroot -c "./config.sh --unattended --url https://github.com/${REPO} --token ${reg_token} --ephemeral --labels self-hosted --replace --name $(hostname)"
 echo '> Runner registered.'
 
-clear_runner() {
-  echo '> Removing runner...'
-  su nonroot -c "./config.sh remove --token ${reg_token}"
+clear_runner() (
+  set -eEuo pipfail
+  echo '> Running runner cleanup...'
+  echo '> Obtaining runner remove token...'
+  remove_token=$(curl -1sSfLX POST -H 'Accept: application/vnd.github+json' -H "Authorization: Bearer ${ACCESS_TOKEN}" -H 'X-GitHub-Api-Version: 2022-11-28' "https://api.github.com/repos/${REPO}/actions/runners/remove-token" | jq .token --raw-output)
+  echo '> Obtained runner remove token.'
+  echo '> Removing the runner...'
+  su nonroot -c "./config.sh remove --token ${remove_token}"
   echo '> Runner removed.'
-}
+  echo '> Running runner cleanup done.'
+)
 on_trap+=(clear_runner)
 
 (
